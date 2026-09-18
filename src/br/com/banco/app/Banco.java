9e199a02bc79a580e01f2ab9789ca005c30a562f
Augusto <augustowrcc@gmail.com>
2026-09-18 10:10:18 -0400

    refactor: merge JOptionPane message and input into single dialog

--- src/br/com/banco/app/Banco.java
+++ src/br/com/banco/app/Banco.java
@@ -1,73 +1,91 @@
 package br.com.banco.app;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 import br.com.banco.model.Conta;
 import br.com.banco.model.Proprietario;
 
 public class Banco {
 
+    static boolean operacoesHabilitadas = false;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
         Conta conta = null;
-
+        String opcao = null;
+        int opcaoInt = 0;
+        int resposta = 0;
         do {
+            opcao = JOptionPane.showInputDialog("Escolha uma opção: \n" +
+                    "1 - Criar conta\n" +
+                    "2 - Sacar\n" +
+                    "3 - Depositar\n" +
+                    "4 - Consultar extrato\n" +
+                    "5 - Area do Gerente\n" +
+                    "6 - Sair");
 
-            System.out.println("Escolha uma opção:");
-            System.out.println("1 - Criar conta");
-            System.out.println("2 - Sacar");
-            System.out.println("3 - Depositar");
-            System.out.println("4 - Consultar extrato");
-            System.out.println("5 - Sair");
+            opcaoInt = Integer.parseInt(opcao);
 
-            opcao = scanner.nextInt();
-
-            switch (opcao){
+            switch (opcaoInt){
                 case 1:
-                    System.out.println("Digite o nome do proprietario:");
-                    String nome = scanner.nextLine();
-                    System.out.println("Digite o CPF do proprietario:");
-                    String cpf = scanner.next();
-                    System.out.println("Digite o endereco do proprietario:");
-                    String endereco = scanner.nextLine();
-                    System.out.println("Digite o email do proprietario:");
-                    String email = scanner.next();
+                    String nome = JOptionPane.showInputDialog("Digite o nome do proprietario:");
+                    String cpf = JOptionPane.showInputDialog("Digite o CPF do proprietario:");
+                    String endereco = JOptionPane.showInputDialog("Digite o endereco do proprietario:");
+                    String email = JOptionPane.showInputDialog("Digite o email do proprietario:");
                     Proprietario proprietario = new Proprietario(nome, cpf, endereco, email);
                     conta = new Conta(proprietario, 0);
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra operação?");
                     break;
                 case 2:
-                    if (conta != null) {
-                        System.out.println("Digite o valor do saque:");
-                        double valorSaque = scanner.nextDouble();
-                        conta.sacar(valorSaque);
-                    } else {
-                        System.out.println("Crie uma conta antes de sacar.");
+                    if(operacoesHabilitadas) {
+                        if (conta != null) {
+                            double valorSaque = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do saque:"));
+                            conta.sacar(valorSaque);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Crie uma conta antes de sacar.");
+                        }
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Saque nao permitido!");
                     }
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra operação?");
                     break;
                 case 3:
-                    if(conta != null){
-                        System.out.println("Digite o valor do deposito:");
-                        double valorDeposito = scanner.nextDouble();
-                        conta.depositar(valorDeposito);
-                    } else {
-                        System.out.println("Crie uma conta antes de depositar.");
+                    if(operacoesHabilitadas) {
+                        if(conta != null){
+                            double valorDeposito = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do deposito:"));
+                            conta.depositar(valorDeposito);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Crie uma conta antes de depositar.");
+                        }
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Deposito nao permitido!");
                     }
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra operação?");
                     break;
                 case 4:
-                    if(conta != null){
-                        System.out.println(conta.extrato());
-                    } else {
-                        System.out.println("Crie uma conta antes de consultar o extrato.");
+                    if(operacoesHabilitadas) {
+                        if (conta != null) {
+                            JOptionPane.showMessageDialog(null, conta.extrato());
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Crie uma conta antes de consultar o extrato.");
+                        }
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Extrato nao permitido!");
                     }
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra operação?");
                     break;
                 case 5:
-                    System.out.println("Saindo...");
+                    operacoesHabilitadas = !operacoesHabilitadas;
+                    if(operacoesHabilitadas){
+                    JOptionPane.showMessageDialog(null, "Permissões liberadas!");
+                    }else{
+                    JOptionPane.showMessageDialog(null, "Permissões bloqueadas!");
+                    }
+                    resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra operação?");
                     break;
-                default:
-                    System.out.println("Opção inválida.");
+                case 6:
+                    JOptionPane.showMessageDialog(null, "Saindo...");
+                    break;
             }
 
-        }while (opcao != 5);
+        }while (resposta != JOptionPane.NO_OPTION && opcaoInt != 6);
 
     }
 }
